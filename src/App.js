2a4e55678ba2b2ced271bc4a568e3a6108537f4f
nsk0826 <nsk.20160401@gmail.com>
2022-01-28 14:38:20 +0900

    mod App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import OcrReader from "./components/OcrReader"
+import SmsSender from "./components/SmsSender"
+import "./App.css"
 
 function App() {
+  const [ocrData, setOcrData] = useState("")
+  
+  // 子コンポーネントからOCRデータをPropsとして受け取る
+  const onReadOcrData = (ocrData) => {
+    setOcrData(ocrData)
+  }
+
+  // 子コンポーネントで別の画像が押されたことをPropsで検知
+  const onRemoveClicked = () => {
+    setOcrData("")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"> Welcome OCR App</header>
+      <OcrReader
+        onReadOcrData={onReadOcrData}
+        onRemoveClicked={onRemoveClicked}
+      />
+      {ocrData && <SmsSender readText={ocrData}/>}
     </div>
   );
 }
