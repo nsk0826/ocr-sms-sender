f58cb5d9e8c604408f1b8ac2796e982869955b13
nsk0826 <nsk.20160401@gmail.com>
2022-01-28 15:01:27 +0900

    add OCR処理

--- src/components/OcrReader.js
+++ src/components/OcrReader.js
@@ -9,4 +9,30 @@ const STATUSES = {
   SUCCEEDED: "OCR処理完了",
 }
 
+function OcrReader({onReadOcrData, onRemoveClicked}){
+  const [selectedImage, setSelectedImage]  = useState(null)
+  const [ocrState, setOcrState] = useState(STATUSES.IDLE)
+  const worker = createWorker()
+
+  // 画像のOCR処理
+  const readImageText = async() => {
+    setOcrState(STATUSES.PENDING)
+    try {
+      await worker.load()
+      // OCRで読み取りたい言語の設定
+      await worker.loadLanguage("jpn")
+      await worker.initialize("jpn")
+      const { data: { text } } = await worker.recognize(selectedImage)
+      await worker.terminate()
+
+      // 日本語テキストはスペースが入ることがあるのでスペースを削除
+      const strippedText = text.replace(/\s+/g, "")
+      onReadOcrData(strippedText)
+      setOcrState(STATUSES.SUCCEEDED)
+    } catch (err) {
+      setOcrState(STATUSES.FAILED)
+    }
+  }
+}
+
 export default OcrReader
